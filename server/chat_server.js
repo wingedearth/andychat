5867dff71adbd33e9566b9fa5d912716b1520f91
Andrew Anissi <aanissi@legacy.com>
2016-04-12 21:44:53 -0700

    1) add assignGuestName function.

--- server/chat_server.js
+++ server/chat_server.js
@@ -37,12 +37,41 @@ exports.listen = function(server) {
 ////////////////////////
 
 function assignGuestName(socket, guestNumber, nickNames, namesUsed) {
-
+	var name = 'Guest' + guestNumber;
+	nickNames[socket.id] = name;
+	socket.emit('nameResult', {
+		success: true,
+		name: name
+	});
+	namesUsed.push(name);
+	return guestNumber + 1;
 }
 
 function joinRoom(socket, room) {
+	socket.join(room);
+	currentRoom[socket.id] = room;
+	socket.emit('joinResult', {room: room});
+	socket.broadcast.to(room).emit('message', {
+		text: nickNames[socket.id] + ' has joined ' + room + '.'
+	});
 
+	var usersInRoom = io.sockets.clients(room);
+	if (usersInRoom.length > 1) {
+		var usersInRoomSummary = 'Participants currently in ' + room + ': ';
+		for (var index in usersInRoom) {
+			var userSocketId = usersInRoom[index].id;
+			if (userSocketId != socket.id) {
+				if (index > 0) {
+					usersInRoomSummary += ', ';
+				}
+			usersInRoomSummary += nickNames[userSocketId];
+			}
+		}
+		usersInRoomSummary += '.';
+		socket.emit('message', {text: usersInRoomSummary});
+	}
 }
+
 
 function handleMsgBroadcast(socket, nickNames) {
 
